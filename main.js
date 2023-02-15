9df735f2305bf600b8a76546e8aeeaa2f00becd6
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-02-15 20:00:46 +0000

    Stuff will work soon

--- main.js
+++ main.js
@@ -28,5 +28,28 @@ document.body.appendChild(imgWrapper);
 
 var createBtn = document.createElement('button');
 createBtn.id = 'catBtn';
+createBtn.textContent = 'new cat'; 
 
 document.body.appendChild(createBtn);
+
+var createScript = document.createElement('script');
+createScript.innerHTML = `
+let btn = document.getElementById('catBtn');
+
+btn.onclick = function newCat(){
+	var catImg = document.getElementById('cat-img');
+	var randNum = Math.floor(Math.random() * 61);
+	var statusCodes = [
+		100, 101, 102, 200, 201, 202, 203, 204, 205, 206,
+		207, 208, 226, 300, 301, 302, 303, 304, 305, 306,
+		307, 308, 400, 401, 402, 403, 404, 405, 406, 407,
+		408, 409, 410, 411, 412, 413, 414, 415, 416, 417,
+		418, 420, 422, 423, 424, 426, 428, 429, 431, 451, 500,
+		501, 502, 503, 504, 505, 506, 507, 508, 510, 511];
+
+	var url = \`https://http.cat/${statusCodes[randNum]}\`;
+
+	catImg.src = url;
+}`
+
+// Currently non functional, soon :tm:
